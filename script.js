fa084804a12be5b92b81b7be4a091277cf0e1452
erhic <eric.gichovi@student.moringaschool.com>
2021-11-14 18:50:02 +0300

    add function, if else statement to calculate

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
 let male = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame']
-let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+let weekDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
 let female = ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama']
 
+
+
 function calculate() {
+    // event.preventDefault()
 
     let date = document.getElementById('date').value;
+    let gender = document.getElementById('gender').value;
+    let daysNo = new Date(date).getDay()
 
 
-
-
-
-
+    if (gender == '' || date == "") {
+        console.log("fill in your date birth and gender")
+    } else if (gender == 'female') {
+        console.log(`You were born on a ${weekDays[daysNo]} and your Akan name is ${female[daysNo]}`)
+    } else if (gender == 'male') {
+        console.log(`You were born on a ${weekDays[daysNo]} and your Akan name is ${male[daysNo]}`)
+    } else {
+        console.log('You have not selected anything');
+    }
 }
