382e2640a67fab752c39d1c94ff1ce82bcf28ca7
erhic <eric.gichovi@student.moringaschool.com>
2021-11-14 15:47:52 +0300

    add function, to calculate the date basedon condition set

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-let dateValue = document.getElementById("date")
 let male = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame']
 let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
 let female = ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama']
 
 function calculate() {
 
-    let date = document.getElementById('date').value
+    let date = document.getElementById('date').value;
+
 
 
 
