0a52197cb30746c7f00ef6737b639f58f5a9ee44
erhic <eric.gichovi@student.moringaschool.com>
2021-11-14 15:45:40 +0300

    add variables

---
+++ script.js
@@ -0,0 +1,14 @@
+let dateValue = document.getElementById("date")
+let male = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame']
+let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+let female = ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama']
+
+function calculate() {
+
+    let date = document.getElementById('date').value
+
+
+
+
+
+}
